0254c7fed5b1f696115b0864f87ca5769256fd39
muradhossain1 <muradssq12@gmail.com>
2024-12-24 20:44:17 +0600

    Create get api server site with details page single data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000
 const app = express();
@@ -59,10 +59,20 @@ async function run() {
         })
 
         //get recent blogs section data limited
-        app.get('/recent-blogs', async(req, res) => {
+        app.get('/recent-blogs', async (req, res) => {
             const result = await blogsCollection.find().limit(6).toArray();
             res.send(result);
         })
+
+        // get a details single data by id from db 
+        app.get('/blog/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await blogsCollection.findOne(query);
+            res.send(result);
+        })
+
+
 
         // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
